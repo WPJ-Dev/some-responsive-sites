1a477725c6156e3b62cb13d4714e9a1017a9ce71
WPJ-Dev <wpruittDev@gmail.com>
2025-12-17 09:01:52 -0600

    video section completed

--- 06-ClassicCars/script.js
+++ 06-ClassicCars/script.js
@@ -7,7 +7,18 @@ document.querySelector('.menu').addEventListener('click', () => {
 document.querySelectorAll('.wrapper').forEach((item) => {
     item.addEventListener('click', () => {
         document.querySelectorAll('.target').forEach((item) => {
-        item.classList.remove('change');
+            item.classList.remove('change');
+        });
     });
-    })
-})
+});
+
+const videos = document.querySelectorAll('.video');
+
+videos.forEach((video) => {
+    video.addEventListener("mouseover", () => {
+        video.play();
+    });
+    video.addEventListener("mouseout", () => {
+        video.pause();
+    });
+});
